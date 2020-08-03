658986f9b6b99c3f5be9d4d84eed7b24fe748297
Thomas Tych <thomas.tych@gmail.com>
2020-08-03 08:29:17 +0200

    blog from rails getting started

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  resources :articles do
+    resources :comments
+  end
+
+  get 'welcome/index'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
